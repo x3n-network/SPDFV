e7cb1da3d5908d89672170f25a45d7da93e36e5c
Joseph Spears <team@x3n.network>
2026-08-29 16:05:51 -0400

    Initial Commit

---
+++ SPDFV/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SPDFV
+//
+//  Created by Joseph Spears on 8/29/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SPDFV/SPDFVApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPDFVApp.swift
+//  SPDFV
+//
+//  Created by Joseph Spears on 8/29/26.
+//
+
+import SwiftUI
+
+@main
+struct SPDFVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
